14452c6db93e0a8d8c54388d8b3005620e866c68
builder <b@e>
2026-08-09 17:26:17 +0000

    oneplus3: drop stale android.hidl.base@1.0 shim
    Defined in both device libhidl/ and hardware/lineage/compat; kept the latter.

--- device.mk
+++ device.mk
@@ -259,7 +259,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
